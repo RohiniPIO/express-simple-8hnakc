80752e3eba3e907ca12a9b4082e46119e5c7ad50
RohiniPIO <rohini.alagarsamy@programmers.io>
2022-06-30 12:59:56 +0530

    1 file modified

--- Task/app.js
+++ Task/app.js
@@ -1,10 +1,73 @@
-const path = require('path');
 const express = require('express');
-const ejs = require('ejs')
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const app = express();
 
-app.listen(5000, () => {
-  console.log('Server is listening to port 5000');
+const app = express();
+const port = process.env.port || 5000;
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api/products', router);
+app.listen(port, () => {
+  console.log(`Server is listening to port: ${port}`);
+});
+
+const products = [
+  {
+    ProductId: 1,
+    Name: 'Kitkat',
+    Cost: 10,
+  },
+
+  {
+    ProductId: 2,
+    Name: 'Dairy Milk',
+    Cost: 40,
+  },
+];
+
+router.get('/', (req, res) => {
+  res.json(products);
+});
+
+//get based on Product id
+router.get('/:Id', (req, res) => {
+  const Id = parseInt(req.parms.Id);
+  const currentProduct = products.filter((x) => x.ProductId === Id)[0];
+  if (currentproduct) {
+    res.json(currentproduct);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+//Validate Product
+validateProduct = (product) => {
+  const message = '';
+  if (Product.ProductId) {
+    message = 'Product Id not found';
+  }
+
+  if (Product.Name) {
+    message = 'Name not found';
+  }
+
+  if (Product.Cost) {
+    message = 'Cost not found';
+  }
+  return message;
+};
+
+//Create Product
+router.post('/', (req, res) => {
+  const priduct = req.body;
+  const isValid = validateProduct(product);
+  if (isValid === '') {
+    products.push(product);
+    console.log(products);
+    res.status(201).send(products);
+  } else {
+    res.statusMessage === isValid;
+    res.sendStatus(404);
+  }
 });
